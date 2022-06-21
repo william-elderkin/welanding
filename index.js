0674f6562ebc227f03489e8fc61e136f6efad0d8
William Elderkin <william_elderkin@protonmail.com>
2022-06-20 20:27:15 -0400

    Add events, change color palette

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const nt = document.querySelector('#nt');
-nt.addEventListener('click', () => {
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Menu of things");
+
+const contact = document.querySelector('#contact');
+contact.onclick = () => alert("Contact info");
+
+const about = document.querySelector('#about');
+about.addEventListener('click', () => {
   alert("Nakey Time?");
 });
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+  alert("Nakey Time?");
+});
+
+function alertFunction() {
+  alert("YAY! YOU DID IT!");
+}
+
+btn4.addEventListener('click', alertFunction);
